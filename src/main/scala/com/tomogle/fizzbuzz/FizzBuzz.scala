c4916613cc24f21324f8b9757dbb9badb6adaf03
tom-ogle <t.ogle.uk@gmail.com>
2016-05-08 19:48:52 +0100

    Added an extensible FizzBuzzer

--- src/main/scala/com/tomogle/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/tomogle/fizzbuzz/FizzBuzz.scala
@@ -1,22 +1,12 @@
 package com.tomogle.fizzbuzz
 
+import com.tomogle.fizzbuzz.Mappings._
 
 object FizzBuzz {
+
   def main(args: Array[String]) {
-    FizzBuzzer.run(1 to 100) map println
-  }
-
-}
-object FizzBuzzer {
-  def run(range: Seq[Int]): Seq[String] = range map assignValue
-
-  private def assignValue(element: Int): String = {
-    val divisibleBy3 = element % 3 == 0
-    val divisibleBy5 = element % 5 == 0
-
-    if (divisibleBy3 && divisibleBy5) "FizzBuzz"
-    else if (divisibleBy3) "Fizz"
-    else if (divisibleBy5) "Buzz"
-    else element.toString
+    val fizzBuzzer = new ExtensibleFizzBuzzer(Seq(divisibleBy3And5, divisibleBy3, divisibleBy5))
+    fizzBuzzer.run(1 to 100) foreach println
   }
 }
+
---
+++ src/test/scala/com/tomogle/fizzbuzz/ExtensibleFizzBuzzerTest.scala
@@ -0,0 +1,41 @@
+package com.tomogle.fizzbuzz
+
+import com.tomogle.fizzbuzz.Mappings._
+import org.scalatest.{FlatSpec, Matchers}
+
+class ExtensibleFizzBuzzerTest extends FlatSpec with Matchers {
+  
+  val fizzBuzzerUnderTest = new ExtensibleFizzBuzzer(Seq(divisibleBy3And5, divisibleBy3, divisibleBy5))
+
+  behavior of "ExtensibleFizzBuzzer#run"
+
+  it should "return a sequence the same length as the input sequence" in {
+    val range = 1 to 10
+    val result = fizzBuzzerUnderTest.run(range)
+    result should have length 10
+  }
+
+  it should "return FizzBuzz for multiple of 3 and 5" in {
+    val input = Seq(15, 30, 45, 60, 75, 90)
+    val result = fizzBuzzerUnderTest.run(input)
+    result should equal(Seq("FizzBuzz", "FizzBuzz", "FizzBuzz","FizzBuzz", "FizzBuzz", "FizzBuzz"))
+  }
+
+  it should "return Fizz for multiples of 3 (and not multiple of 5)" in {
+    val input = Seq(3, 6, 9, 12)
+    val result = fizzBuzzerUnderTest.run(input)
+    result should equal(Seq("Fizz", "Fizz", "Fizz", "Fizz"))
+  }
+
+  it should "return Buzz for multiples of 5 (and not multiple of 3)" in {
+    val input = Seq(5, 10, 20, 100)
+    val result = fizzBuzzerUnderTest.run(input)
+    result should equal(Seq("Buzz", "Buzz", "Buzz", "Buzz"))
+  }
+
+  it should "return the original number for non-multiples of 3 or 5" in {
+    val input = Seq(1, 2, 4, 7)
+    val result = fizzBuzzerUnderTest.run(input)
+    result should equal(Seq("1", "2", "4", "7"))
+  }
+}
